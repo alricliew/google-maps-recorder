91d103d5640ece7afa55717773545738be4b169a
Alric <alricliew@gmail.com>
2026-04-22 12:44:27 +0800

    Remove node_modules and dist from tracking

--- dist/utils.js
+++
@@ -1,7 +0,0 @@
-export function toCSV(data) {
-    if (!data.length)
-        return "";
-    const headers = Object.keys(data[0]);
-    const rows = data.map(row => headers.map(h => JSON.stringify(row[h] ?? "")).join(","));
-    return [headers.join(","), ...rows].join("\n");
-}
